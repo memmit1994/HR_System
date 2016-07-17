19d42b99f63839b37c07ae49a14991ec1a3e006e
memmit1994 <memmit1994@gmail.com>
2016-07-18 01:49:23 +0200

    volunteer dashboard

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,4 +1,7 @@
 class Session < ActiveRecord::Base
   has_and_belongs_to_many :users, join_table: "tasks"
   belongs_to :event
+  def start_time
+    start
+  end
 end
--- app/models/shift.rb
+++ app/models/shift.rb
@@ -2,5 +2,7 @@ class Shift < ActiveRecord::Base
   has_and_belongs_to_many :users, join_table: "tasks"
   belongs_to :event
   has_and_belongs_to_many :users, join_table: "availables"
-
+  def start_time
+    start
+  end
 end
